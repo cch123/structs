abcb758e678e0ada0ace98c70113cb9578784aea
Xargin <cao1988228@163.com>
2016-04-05 21:00:21 +0800

    add some test

--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -2,11 +2,52 @@ package skiplist
 
 import "testing"
 
+var strArr = []string{"a", "b", "name", "xargin"}
+
+func compare(a interface{}, b interface{}) int {
+	if a.(string) > b.(string) {
+		return 1
+	} else if a.(string) < b.(string) {
+		return -1
+	}
+	return 0
+}
+
 func TestInsert(t *testing.T) {
-	t.Fatalf("oh no %d\n", 1)
+	var x *Skiplist = SkiplistCreate(compare)
+	var insRes *SkiplistNode
+	for _, str := range strArr {
+		insRes = x.SkiplistInsert(str)
+		if insRes == nil {
+			t.Errorf("insert %s failed\n", str)
+		}
+	}
 }
 
 func TestDelete(t *testing.T) {
+	var x *Skiplist = SkiplistCreate(compare)
+	var insRes *SkiplistNode
+	for _, str := range strArr {
+		insRes = x.SkiplistInsert(str)
+		if insRes == nil {
+			t.Errorf("insert %s failed\n", str)
+		}
+	}
+
+	x.Print()
+	var lastLength = x.Length
+	var delRes int
+	for _, str := range strArr {
+		delRes = x.SkiplistDelete(str)
+		if delRes != 1 || lastLength != x.Length+1 {
+			t.Errorf("delete str %s error !\n", str)
+		}
+	}
+
+	if x.Length != 0 {
+		t.Errorf("after delete all there cannot be any element left\n")
+	}
+	x.Print()
 }
 
 func TestPopHead(t *testing.T) {
