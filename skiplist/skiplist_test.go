782e5c3df46070f1c5dcbaed5fda3528df4d888f
Xargin <cao1988228@163.com>
2016-04-05 12:38:07 +0800

    add test 10%

---
+++ skiplist/skiplist_test.go
@@ -0,0 +1,25 @@
+package skiplist
+
+import "testing"
+
+func TestInsert(t *testing.T) {
+	t.Fatalf("oh no %d\n", 1)
+}
+
+func TestDelete(t *testing.T) {
+}
+
+func TestPopHead(t *testing.T) {
+}
+
+func TestPopTail(t *testing.T) {
+}
+
+func TestLength(t *testing.T) {
+}
+
+func TestCreate(t *testing.T) {
+}
+
+func TestDuplicateNode(t *testing.T) {
+}
